e54f4dc9caa829ec089e4508af602677d73bf6cd
juandesi <juan.desimoni@mulesoft.com>
2019-01-03 17:43:07 -0300

    MDM-80: Metadatatype serialization should not be so heavy

--- src/main/kotlin/org/mule/wsdl/parser/serializer/WsdlModelSerializer.kt
+++ src/main/kotlin/org/mule/wsdl/parser/serializer/WsdlModelSerializer.kt
@@ -2,14 +2,14 @@ package org.mule.wsdl.parser.serializer
 
 import com.google.gson.GsonBuilder
 import org.mule.metadata.api.model.MetadataType
-import org.mule.metadata.persistence.MetadataTypeGsonTypeAdapter
+import org.mule.metadata.persistence.reduced.ReducedMetadataTypeGsonTypeAdapter
 import org.mule.wsdl.parser.model.WsdlModel
 
 object WsdlModelSerializer {
 
   fun serialize(model: WsdlModel, pretty: Boolean = false): String {
     val gson = GsonBuilder()
-    gson.registerTypeAdapter(MetadataType::class.java, MetadataTypeGsonTypeAdapter())
+    gson.registerTypeAdapter(MetadataType::class.java, ReducedMetadataTypeGsonTypeAdapter())
     if (pretty) {
       gson.setPrettyPrinting()
     }
@@ -18,7 +18,7 @@ object WsdlModelSerializer {
 
   fun deserialize(model: String): WsdlModel {
     return GsonBuilder()
-      .registerTypeAdapter(MetadataType::class.java, MetadataTypeGsonTypeAdapter())
+      .registerTypeAdapter(MetadataType::class.java, ReducedMetadataTypeGsonTypeAdapter())
       .create()
       .fromJson(model, WsdlModel::class.java)
   }
